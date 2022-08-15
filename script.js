4bc3535526555e4621b185f522e93a3a198c49e8
Aazam Thakur <aazamthakur@gmail.com>
2022-08-15 14:40:27 +0530

    Added Carasoul

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ const nav = document.querySelector('nav');
 bar.addEventListener("click", ()=>{
     nav.classList.toggle("nav");
 })
+let counter = 1;
 
+setInterval(function(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if(counter > 4){
+    counter = 1;
+  }
+}, 5000);
